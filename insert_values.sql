74b74eb531b3329e8184eb6a578dbe9b920e2c29
Dario Ferrero <dario.ferrero@edu.unito.it>
2020-05-27 16:04:02 +0200

    Bozza popolamento BD

--- insert_values.sql
+++ insert_values.sql
@@ -1,4 +1,27 @@
+-- DA RISPETTARE ORDINE DI DEFINIZIONE (in create table)
 -- Contenuto (trama, paese, genere, distribuzione, anno, titolo, voto_medio, durata)
 insert into Contenuto values ('Trama di Bad Boys for Life', 'USA', 'Azione', 'Sony Pictures Italia / Warner Bros. Pictures Italia', 2020, 'Bad Boys for Life', null, 123);
+insert into Contenuto values ('Trama di Sonic - Il Film', 'Giappone, USA, Canada', 'Azione, Animazione, Avventura', 'Paramount Pictures', 2020, 'Sonic - Il Film', null, 100);
+insert into Contenuto values ('Trama di Jojo Rabbit', 'Germania, USA', 'Commedia, Drammatico', 'Walt Disney Italia / 20th Century Fox', 2020, 'Jojo Rabbit', null, 108);
+insert into Contenuto values ('Trama di Doolittle', 'USA', 'Commedia', 'Universal Pictures', 2020, 'Doolittle', null, 106);
+
+-- Episodio ()
+insert into Episodio values ();
+
+-- Utente ()
+insert into Utente values ();
+
+-- Notizia ()
+insert into Notizia values ();
+
+-- Star ()
+insert into Star values ();
+
+-- Cinema ()
+insert into Cinema values ();
+
+-- Piattaforma ()
+insert into Piattaforma values ();
+
 
 
